69c2373b10e872f7308a54a2edbba07a2875461c
Kai Sellgren <kaisellgren@gmail.com>
2014-10-06 19:25:51 +0300

    Make GitError to derive Show trait

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,8 @@
 use std::fmt;
 
+#[deriving(Show)]
 pub enum GitError {
     CorruptRepository(&'static str),
     CorruptCommit(&'static str),
     NotFound,
-}
-
-impl fmt::Show for GitError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            CorruptCommit(s) => write!(f, "CorruptCommit: {}", s),
-            CorruptRepository(s) => write!(f, "CorruptRepository: {}", s),
-            NotFound => write!(f, "NotFound"),
-        }
-    }
 }
